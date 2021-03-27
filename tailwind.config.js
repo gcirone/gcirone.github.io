676a86b63178eb42460d64772bd49742c1999ce5
Gianluca Cirone <gianluca.cirone@gmail.com>
2021-03-27 17:09:35 +0100

    Add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: {
+    enabled: process.env.WEBPACK_DEV_SERVER === 'true' && process.argv.join(' ').indexOf('build') !== -1,
+    content: [
+      "./apps/**/*.{html,ts,scss}",
+      "./libs/**/*.{html,ts,scss}"
+    ]
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
