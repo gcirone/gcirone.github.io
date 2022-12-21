6f08e8660da33bc2ea22aee3782948aed84ad679
Gianluca Cirone <gianluca.cirone@gmail.com>
2022-12-21 18:03:28 +0100

    reset workspace

--- README.md
+++
@@ -1,2 +0,0 @@
-# gcirone.com
-Gianluca Cirone Website [gcirone.com](http://gcirone.com/)
--- jest.config.ts
+++
@@ -1,6 +0,0 @@
-const { getJestProjects } = require('@nrwl/jest');
-
-export default {
-  cacheDirectory: `${process.cwd()}/node_modules/.cache/jest`,
-  projects: getJestProjects(),
-};
