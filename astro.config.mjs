e13cc0bed43bb7325b0abddb6f59cc2d3d51f1e5
Gianluca Cirone <gianluca.cirone@gmail.com>
2024-10-12 22:30:05 +0200

    Add view transition api

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  base: '/staging',
+  base: '/',
   trailingSlash: 'always',
   compressHTML: false,
   scopedStyleStrategy: 'where',
