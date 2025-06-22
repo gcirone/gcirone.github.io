faa48d215d9e5ee7514d596fb06ac2848ced6026
Gianluca <gianluca.cirone@gmail.com>
2025-06-22 22:57:32 +0200

    Update nx workspace

--- apps/gc-website/src/app/middleware.ts
+++ apps/gc-website/src/app/middleware.ts
@@ -2,4 +2,4 @@ import { deviceType } from '~/app/middleware/device-type';
 import { serverLogger } from '~/app/middleware/server-logger';
 import { cacheControl } from '~/app/middleware/cache-control';
 
-export const middleware = [deviceType, serverLogger, cacheControl];
+export const middleware = [deviceType, serverLogger, cacheControl] as never[];
